c8ba3248c2db1b2394d30fc19c5310d04be9f9fa
wangyi <1521966572@qq.com>
2024-03-18 13:39:34 +0800

    initial

---
+++ test.py
@@ -0,0 +1,9 @@
+# import matplotlib.pyplot as plt
+# fig=plt.figure(num=1,figsize=(4,4))
+# plt.plot([1,2,3,4],[1,2,3,4])
+# plt.show()
+# fig.savefig("plot.png")
+a = [i for i in range(10)]
+b = [a[-2:]]
+print(a)
+print(b)
